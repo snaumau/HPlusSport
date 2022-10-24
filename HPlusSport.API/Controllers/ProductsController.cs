f5dc0585400ba028d068c4f59b96155531149171
Siarhei Naumau <s.naumau@gmail.com>
2022-10-24 17:52:29 +0300

    feat: add empty controller for products

---
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HPlusSport.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProduct()
+        {
+            return "OK";
+        }
+    }
+}
