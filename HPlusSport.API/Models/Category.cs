90707e63bd806a45526335d73a8adcecc368003d
Siarhei Naumau <s.naumau@gmail.com>
2022-10-24 17:59:22 +0300

    feat: add models

---
+++ HPlusSport.API/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace HPlusSport.API.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;2
+
+        public virtual List<Product>? Products { get; set; }
+    }
+}
