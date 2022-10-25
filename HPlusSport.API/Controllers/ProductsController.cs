ffb7511b8553ca40f9f7af4a851ed917a11e9283
Siarhei Naumau <s.naumau@gmail.com>
2022-10-25 19:28:35 +0300

    feat: add handling errors in the controller

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -26,6 +26,10 @@ namespace HPlusSport.API.Controllers
         public ActionResult GetProduct(int id)
         {
             var product = _context?.Products?.Find(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
             return Ok(product);
         }
     }
