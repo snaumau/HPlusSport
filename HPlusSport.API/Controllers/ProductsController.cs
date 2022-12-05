095d85ba238dc9883d0b43201d5bc049ad24d090
Siarhei Naumau <s.naumau@gmail.com>
2022-12-05 21:42:01 +0300

    feat: implement searching

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -33,6 +33,19 @@ namespace HPlusSport.API.Controllers
                 products = products.Where(
                     p => p.Price <= queryParameters.MaxPrice.Value);
             }
+
+            if (!string.IsNullOrEmpty(queryParameters.Sku))
+            {
+                products = products.Where(
+                    p => p.Sku == queryParameters.Sku);
+            }
+
+            if (!string.IsNullOrEmpty(queryParameters.Name))
+            {
+                products = products.Where(
+                    p => p.Name.ToLower().Contains(
+                        queryParameters.Name.ToLower()));
+            }
             
             products = products
                 .Skip(queryParameters.Size * (queryParameters.Page - 1))
